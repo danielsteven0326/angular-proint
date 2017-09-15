501e1377273c5ca7bfd72d20a24e274dd28b5651
Daniel González <dsteven.gonzalez@udea.edu.co>
2017-09-15 00:20:57 -0500

    commit para corregir un error de digitación en una coma (,) en styles desde feat_multiple_components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,18 @@
 import { Component } from '@angular/core';
 
-export class Hero {
-  id: number;
-  name: string;
-}
+import { Hero } from './hero';
 
-const HEROES: Hero[] =[
-  { id: 11, name: 'Mr. Nice'},
-  { id: 12, name: 'Narco'},
-  { id: 13, name: 'Bombasto'},
-  { id: 14, name: 'Celeritas'},
-  { id: 15, name: 'Magneta'},
-  { id: 16, name: 'RubberMan'},
-  { id: 17, name: 'Dynama'},
-  { id: 18, name: 'Dr IQ'},
-  { id: 19, name: 'Magma'},
-  { id: 20, name: 'Tornado'},
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
 ];
 
 @Component({
@@ -24,14 +21,14 @@ const HEROES: Hero[] =[
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes" 
+      <li *ngFor="let hero of heroes"
         [class.selected]="hero === selectedHero"
         (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
     <hero-detail [hero]="selectedHero"></hero-detail>
-    `
+  `,
   styles: [`
     .selected {
       background-color: #CFD8DC !important;
@@ -84,10 +81,10 @@ const HEROES: Hero[] =[
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  heroes=HEROES;
+  heroes = HEROES;
   selectedHero: Hero;
 
-  onSelect(hero:Hero):void{
-    this.selectedHero=hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 }
